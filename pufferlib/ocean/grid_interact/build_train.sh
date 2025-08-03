f084eb79fe1dd9f6ebcad8a776ae5ceef6d02915
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-03 12:27:54 -0700

    SPlit into two scripts

--- pufferlib/ocean/grid_interact/build_train.sh
+++ pufferlib/ocean/grid_interact/build_train.sh
@@ -1,34 +1,10 @@
-echo "Building grid_interact..."
-bash scripts/build_ocean.sh grid_interact 
+sh build.sh
 if [ $? -ne 0 ]; then
-  echo "Unable to build grid_interact"
   exit 1
 fi
 
-echo "Building ext for training"
-python setup.py build_ext --inplace --force 
+sh train.sh
 if [ $? -ne 0 ]; then
-  echo "Unable to build ext"
   exit 1
 fi
 
-echo "Training using: "
-cat ./pufferlib/config/ocean/grid_interact.ini
-python -m pufferlib.pufferl train puffer_grid_interact --train.device cuda   
-if [ $? -ne 0 ]; then
-  echo "Unable to train"
-  exit 1
-fi
-
-echo "Done training..."
-
-
-echo "Eval'ing trained model..."
-
-python -m pufferlib.pufferl eval puffer_grid_interact --train.device cuda --load-model-path latest    
-
-
-
-
-echo "Exporting weights..."
-sh pufferlib/ocean/grid_interact/export_weights.sh
