a58bb7cd7c511038a50ac935bd2f2290742f0c1a
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-03 10:03:33 -0700

    Add train/eval script

---
+++ pufferlib/ocean/grid_interact/build_train.sh
@@ -0,0 +1,25 @@
+echo "Building grid_interact..."
+bash scripts/build_ocean.sh grid_interact 
+if [ $? -ne 0 ]; then
+  echo "Unable to move weights file"
+  exit 1
+fi
+
+echo "Training using: "
+cat ./pufferlib/config/ocean/grid_interact.ini
+python -m pufferlib.pufferl train puffer_grid_interact --train.device cuda   
+if [ $? -ne 0 ]; then
+  echo "Unable to train"
+  exit 1
+fi
+
+echo "Done training..."
+
+sh pufferlib/ocean/grid_interact/export_weights.sh
+
+echo "Eval'ing trained model..."
+
+python -m pufferlib.pufferl eval puffer_grid_interact --train.device cuda --load-model-path latest    
+
+
+
--- pufferlib/ocean/grid_interact/export_weights.sh
+++ pufferlib/ocean/grid_interact/export_weights.sh
@@ -1,4 +1,13 @@
 
 python -m pufferlib.pufferl export puffer_grid_interact --load-model-path latest
+if [ $? -ne 0 ]; then
+  echo "Export failed"
+  exit 1
+fi
+
 mv puffer_grid_interact_weights.bin ./resources/grid_interact/grid_interact_weights.bin
+if [ $? -ne 0 ]; then
+  echo "Unable to move weights file"
+  exit 1
+fi
 echo "Weights copied to ./resources/grid_interact/grid_interact_weights.bin"
