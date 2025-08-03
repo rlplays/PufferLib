4ec028b0da5c9760abefa31ae4eea8a968a838ff
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-03 10:05:44 -0700

    Oops, didn't build it correctly

--- pufferlib/ocean/grid_interact/build_train.sh
+++ pufferlib/ocean/grid_interact/build_train.sh
@@ -1,7 +1,14 @@
 echo "Building grid_interact..."
 bash scripts/build_ocean.sh grid_interact 
 if [ $? -ne 0 ]; then
-  echo "Unable to move weights file"
+  echo "Unable to build grid_interact"
+  exit 1
+fi
+
+echo "Building ext for training"
+python setup.py build_ext --inplace --force 
+if [ $? -ne 0 ]; then
+  echo "Unable to build ext"
   exit 1
 fi
 
