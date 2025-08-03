8db497ea97fe9d4c49feeb627c2645c2ba522b33
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-03 10:18:16 -0700

    Increase fov too?

--- pufferlib/ocean/grid_interact/grid_interact.c
+++ pufferlib/ocean/grid_interact/grid_interact.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
         .width = 1000,
         .height = 1000,
         .cell_size = 40,
-        .fov = 20,
+        .fov = 40,
         .num_rewards = 50,
         .cell_types = NUM_CELL_TYPES, // W, #, R, G, EMPTY, P
     };
