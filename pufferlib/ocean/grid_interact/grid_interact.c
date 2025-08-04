a3f5e02829f4a6cd84813152ef30f3c0ce4d5971
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-04 13:19:33 -0700

    Add gifs

--- pufferlib/ocean/grid_interact/grid_interact.c
+++ pufferlib/ocean/grid_interact/grid_interact.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
   LinearLSTM *net = NULL;
   if (use_trained_model) {
     weights = load_weights("resources/grid_interact/grid_interact_weights.bin",
-                           299654);
+                           262790);
     net = make_linearlstm(weights, 1, num_obs, logit_sizes, 1);
   }
 
