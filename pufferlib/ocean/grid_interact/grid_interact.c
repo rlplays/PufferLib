ad2e89ce10d4830ddd0f2a6dfcfc03f947cae998
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-05 00:44:14 -0700

    Auto-format

--- pufferlib/ocean/grid_interact/grid_interact.c
+++ pufferlib/ocean/grid_interact/grid_interact.c
@@ -9,55 +9,49 @@
 #include "puffernet.h"
 
 int main(int argc, char **argv) {
-  GridInteractEnv env = {.width = 1600,
-                         .height = 1600,
-                         .cell_size = 100,
-                         .fov = 7,
-                         .num_rewards = 25,
-                         .cell_types = NUM_CELL_TYPES, // W, #, R, G, P (Exclude EMPTY)
-                         .set_max_moves = 5000000
-                         };
+    GridInteractEnv env = {.width = 1600, .height = 1600, .cell_size = 100, .fov = 7, .num_rewards = 25,
+                           .cell_types = NUM_CELL_TYPES, // W, #, R, G, P (Exclude EMPTY)
+                           .set_max_moves = 5000000};
 
-  // Helps keep the number of observations constant regardless of the number of
-  // agents/goals/rewards etc.
-  int num_obs = get_num_obs(&env);
+    // Helps keep the number of observations constant regardless of the number of
+    // agents/goals/rewards etc.
+    int num_obs = get_num_obs(&env);
 
-  int logit_sizes[1] = {5};
+    int logit_sizes[1] = {5};
 
-  bool use_trained_model = argc > 1 && strcmp(argv[1], "trained") == 0;
+    bool use_trained_model = argc > 1 && strcmp(argv[1], "trained") == 0;
 
-  // Weights are exported by running puffer export
-  Weights *weights = NULL;
-  LinearLSTM *net = NULL;
-  if (use_trained_model) {
-    weights = load_weights("resources/grid_interact/grid_interact_weights.bin",
-                           330374);
-    net = make_linearlstm(weights, 1, num_obs, logit_sizes, 1);
-  }
-
-  allocate(&env, use_trained_model);
-
-  // Always call reset and render first
-  c_reset(&env);
-  c_render(&env);
-
-  int frame_index = 0;
-  while (!WindowShouldClose()) {
+    // Weights are exported by running puffer export
+    Weights *weights = NULL;
+    LinearLSTM *net = NULL;
     if (use_trained_model) {
-      // Only run the model at a lower fps to give the user a chance to react.
-      if (frame_index % 1 == 0) {
-        forward_linearlstm(net, env.observations, env.actions);
-      }
+        weights = load_weights("resources/grid_interact/grid_interact_weights.bin", 330374);
+        net = make_linearlstm(weights, 1, num_obs, logit_sizes, 1);
     }
-    c_step(&env);
-    c_render(&env);
-    ++frame_index;
-  }
 
-  // Try to clean up after yourself
-  if (use_trained_model) {
-    free_linearlstm(net);
-    free(weights);
-  }
-  free_allocated(&env);
+    allocate(&env, use_trained_model);
+
+    // Always call reset and render first
+    c_reset(&env);
+    c_render(&env);
+
+    int frame_index = 0;
+    while (!WindowShouldClose()) {
+        if (use_trained_model) {
+            // Only run the model at a lower fps to give the user a chance to react.
+            if (frame_index % 1 == 0) {
+                forward_linearlstm(net, env.observations, env.actions);
+            }
+        }
+        c_step(&env);
+        c_render(&env);
+        ++frame_index;
+    }
+
+    // Try to clean up after yourself
+    if (use_trained_model) {
+        free_linearlstm(net);
+        free(weights);
+    }
+    free_allocated(&env);
 }
