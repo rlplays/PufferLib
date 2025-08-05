bc39e9d169e7e73d885af2da03126cc3ac5a6f48
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-05 00:11:47 -0700

    Too big

--- pufferlib/ocean/grid_interact/grid_interact.c
+++ pufferlib/ocean/grid_interact/grid_interact.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv) {
   GridInteractEnv env = {.width = 1600,
                          .height = 1600,
                          .cell_size = 100,
-                         .fov = 10,
-                         .num_rewards = 10,
+                         .fov = 5,
+                         .num_rewards = 25,
                          .cell_types = NUM_CELL_TYPES, // W, #, R, G, P (Exclude EMPTY)
                          .set_max_moves = 100000
                          };
