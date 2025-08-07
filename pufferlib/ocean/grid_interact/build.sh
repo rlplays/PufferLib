6f2d80aad1ebcf3526987a0f2abd3fab35b02153
Peru S (WSLDesktop) <perumaal@rlplays.com>
2025-08-07 10:26:14 -0700

    Hmm are we even using the correct model?

--- pufferlib/ocean/grid_interact/build.sh
+++ pufferlib/ocean/grid_interact/build.sh
@@ -1,5 +1,6 @@
 echo "Building grid_interact..."
-bash scripts/build_ocean.sh grid_interact fast
+# bash scripts/build_ocean.sh grid_interact fast
+bash scripts/build_ocean.sh grid_interact
 if [ $? -ne 0 ]; then
   echo "Unable to build grid_interact"
   exit 1
