eb442ff29a9e6f0107de458c810aa25076b6cffa
Peru S <perumaal@rlplays.com>
2025-10-13 21:59:00 -0700

    Add a multi-threaded `vec_step` that can run N threads in parallel with zero context switches and no allocs

--- pufferlib/ocean/env_binding.h
+++ pufferlib/ocean/env_binding.h
@@ -1,6 +1,11 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
+#ifdef PUFFERLIB_NUM_THREADS
+#include <pthread.h>
+#include <stdatomic.h>
+#endif
+
 // Forward declarations for env-specific functions supplied by user
 static int my_log(PyObject* dict, Log* log);
 static int my_init(Env* env, PyObject* args, PyObject* kwargs);
@@ -262,7 +267,144 @@ static PyObject* env_put(PyObject* self, PyObject* args, PyObject* kwargs) {
 typedef struct {
     Env** envs;
     int num_envs;
+#ifdef PUFFERLIB_NUM_THREADS
+  atomic_int work_index;
+  atomic_int num_running_threads;
+  volatile int num_threads;
+  pthread_cond_t wake_cnd;
+  pthread_mutex_t main_mtx;
+  pthread_cond_t done_cnd;
+  pthread_t* threads;
+#endif
 } VecEnv;
+
+#ifdef PUFFERLIB_NUM_THREADS
+static void* c_threadstep(void* arg)
+{
+  VecEnv* vec_env = (VecEnv*)arg;
+  pthread_mutex_t mtx;
+  pthread_mutex_init(&mtx, NULL);
+  pthread_cond_t* wake = &vec_env->wake_cnd;
+  pthread_cond_t* done = &vec_env->done_cnd;
+  atomic_int* work_index = &vec_env->work_index;
+  int index;
+  atomic_fetch_add(&vec_env->num_running_threads, 1);
+  while (1)
+  {
+    // Wait for work
+    pthread_mutex_lock(&mtx);
+    pthread_cond_wait(wake, &mtx);
+    pthread_mutex_unlock(&mtx);
+
+    // Got work.
+    if (vec_env->num_threads == 0) { break; } // Exit thread gracefully.
+    atomic_fetch_add(&vec_env->num_running_threads, 1);
+    do
+    {
+      // This is important: Go do a bunch of work in our thread, without context switches or locks
+      // or any new allocs. This is the main speedup and core to ensuring the threads do as little work
+      // as part of their main loop as possible. We can afford to this as the load balancing naturally happens
+      // with mutually exclusive index values spread across threads.
+      // TODO(perumaal): Prevent (cache) fragmentation by dividing/carving the envs array into chunks and
+      //                 having each thread work on a chunk.
+      index = atomic_fetch_sub(work_index, 1);
+      if (index >= 0) { c_step(vec_env->envs[index]); }
+    }
+    while (index > 0);
+    atomic_fetch_sub(&vec_env->num_running_threads, 1);
+    pthread_cond_signal(done);
+  }
+  pthread_mutex_destroy(&mtx);
+  return NULL;
+}
+
+static void c_vecclose(VecEnv* vec_env)
+{
+  if (vec_env->num_threads == 0) { return; }
+  if (vec_env->threads)
+  {
+    const int num_threads = vec_env->num_threads;
+    vec_env->num_threads = 0; // Signal to threads to exit.
+    pthread_cond_broadcast(&vec_env->wake_cnd);
+    for (int i = 0; i < num_threads; ++i) { 
+      pthread_join(vec_env->threads[i], NULL);
+    }
+    pthread_cond_destroy(&vec_env->wake_cnd);
+    pthread_mutex_destroy(&vec_env->main_mtx);
+    pthread_cond_destroy(&vec_env->done_cnd);
+    free(vec_env->threads);
+    vec_env->threads = NULL;
+  }
+  vec_env->num_threads = 0;
+}
+
+static int c_vecinit(VecEnv* vec_env)
+{
+  vec_env->num_threads = PUFFERLIB_NUM_THREADS;
+  // Initialize pthread primitives
+  if (pthread_mutex_init(&vec_env->main_mtx, NULL) != 0) {
+    return 0; // Failure
+  }
+  if (pthread_cond_init(&vec_env->wake_cnd, NULL) != 0) {
+    pthread_mutex_destroy(&vec_env->main_mtx);
+    return 0; // Failure
+  }
+  if (pthread_cond_init(&vec_env->done_cnd, NULL) != 0) {
+    pthread_mutex_destroy(&vec_env->main_mtx);
+    pthread_cond_destroy(&vec_env->wake_cnd);
+    return 0; // Failure
+  }
+  
+  // Allocate threads
+  vec_env->threads = (pthread_t*)malloc(vec_env->num_threads * sizeof(pthread_t));
+  if (!vec_env->threads) {
+    pthread_mutex_destroy(&vec_env->main_mtx);
+    pthread_cond_destroy(&vec_env->wake_cnd);
+    pthread_cond_destroy(&vec_env->done_cnd);
+    return 0; // Failure
+  }
+  
+  atomic_store(&vec_env->num_running_threads, 0);
+  atomic_store(&vec_env->work_index, -1);
+
+  // Create threads
+  for (int i = 0; i < vec_env->num_threads; ++i)
+  {
+    if (pthread_create(&vec_env->threads[i], NULL, c_threadstep, vec_env) != 0) {
+      // Cleanup on failure
+      vec_env->num_threads = i; // Only join the threads we've created
+      c_vecclose(vec_env);
+      return 0; // Failure
+    }
+  }
+
+  // Wait for all threads to initialize
+  while (atomic_load(&vec_env->num_running_threads) < vec_env->num_threads) {}
+  atomic_store_explicit(&vec_env->num_running_threads, 0, memory_order_relaxed);
+  return 1; // Success
+}
+
+static int c_vecstep(VecEnv* vec_env)
+{
+  if (vec_env->num_threads == 0 || atomic_load(&vec_env->work_index) >= 0) { return 0; }
+  
+  // Setup work for all the threads and wake them up at once.
+  atomic_store_explicit(&vec_env->work_index, vec_env->num_envs-1, memory_order_relaxed);
+
+  // Signal that there is new work to be done.
+  pthread_cond_broadcast(&vec_env->wake_cnd);
+
+  // Wait for all threads to finish
+  pthread_mutex_lock(&vec_env->main_mtx);
+  pthread_cond_wait(&vec_env->done_cnd, &vec_env->main_mtx);
+  pthread_mutex_unlock(&vec_env->main_mtx);
+  
+  while (atomic_load(&vec_env->num_running_threads) > 0) {}
+  atomic_store_explicit(&vec_env->work_index, -1, memory_order_relaxed);
+  return 1;
+}
+#endif
+
 
 static VecEnv* unpack_vecenv(PyObject* args) {
     PyObject* handle_obj = PyTuple_GetItem(args, 0);
@@ -441,6 +583,13 @@ static PyObject* vec_init(PyObject* self, PyObject* args, PyObject* kwargs) {
         }
     }
 
+#ifdef PUFFERLIB_NUM_THREADS
+    if (!c_vecinit(vec)) {
+        PyErr_SetString(PyExc_RuntimeError, "Failed to initialize vec env threads");
+        return NULL:
+    }
+#endif
+
     Py_DECREF(kwargs);
     return PyLong_FromVoidPtr(vec);
 }
@@ -476,6 +625,12 @@ static PyObject* vectorize(PyObject* self, PyObject* args) {
         vec->envs[i] = (Env*)PyLong_AsVoidPtr(handle_obj);
     }
 
+#ifdef PUFFERLIB_NUM_THREADS
+    if (!c_vecinit(vec)) {
+        PyErr_SetString(PyExc_RuntimeError, "Failed to initialize vec env threads");
+        return NULL;
+    }
+#endif
     return PyLong_FromVoidPtr(vec);
 }
 
@@ -517,9 +672,13 @@ static PyObject* vec_step(PyObject* self, PyObject* arg) {
         return NULL;
     }
 
+#ifdef PUFFERLIB_NUM_THREADS
+    c_vecstep(vec); // TODO: Error handling?
+#else
     for (int i = 0; i < vec->num_envs; i++) {
         c_step(vec->envs[i]);
     }
+#endif    
     Py_RETURN_NONE;
 }
 
@@ -603,6 +762,9 @@ static PyObject* vec_close(PyObject* self, PyObject* args) {
         return NULL;
     }
 
+#ifdef PUFFERLIB_NUM_THREADS
+    c_vecclose(vec);
+#endif
     for (int i = 0; i < vec->num_envs; i++) {
         c_close(vec->envs[i]);
         free(vec->envs[i]);
