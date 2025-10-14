bfe21003ee584dc4b42f2a109156d1324dd4bf26
Peru S <perumaal@rlplays.com>
2025-10-14 15:18:51 -0700

    Few minor fixes

--- pufferlib/ocean/env_binding.h
+++ pufferlib/ocean/env_binding.h
@@ -303,11 +303,11 @@ typedef struct {
 static void* c_threadstep(void* arg)
 {
     VecEnv* vec_env = (VecEnv*)arg;
-    
+
     pthread_mutex_t mtx;
     pthread_mutex_init(&mtx, NULL);
     pthread_cond_t* wake = &vec_env->thread_data->wake_cnd;
-    
+
     atomic_int* work_index = &vec_env->thread_data->work_index;
     atomic_int* num_running_threads = &vec_env->thread_data->num_running_threads;
     volatile int* num_threads = &vec_env->thread_data->num_threads;
@@ -319,7 +319,7 @@ static void* c_threadstep(void* arg)
         pthread_mutex_lock(&mtx);
         pthread_cond_wait(wake, &mtx);
         pthread_mutex_unlock(&mtx);
-        
+
         if (*num_threads <= 0) { break; } // Exit thread gracefully.
 
         // Got work to do now.
@@ -336,7 +336,7 @@ static void* c_threadstep(void* arg)
         while (index > 0);
         atomic_fetch_sub(num_running_threads, 1);
     }
-    pthread_mutex_destroy(&mtx);    
+    pthread_mutex_destroy(&mtx);
     return NULL;
 }
 
@@ -349,18 +349,17 @@ static void c_vecclose(VecEnv* vec_env)
         atomic_store(&vec_env->thread_data->work_index, -1);
         vec_env->thread_data->num_threads = 0; // Signal to threads to exit
         pthread_cond_broadcast(&vec_env->thread_data->wake_cnd);
-        // Wait for them to exit.  
+        // Wait for them to exit.
         while (atomic_load(&vec_env->thread_data->num_running_threads) > 0) {}
 
         for (int i = 0; i < num_threads; ++i)
         {
             pthread_join(vec_env->thread_data->threads[i], NULL);
         }
-        pthread_cond_destroy(&vec_env->thread_data->wake_cnd);        
+        pthread_cond_destroy(&vec_env->thread_data->wake_cnd);
         free(vec_env->thread_data->threads);
         vec_env->thread_data->threads = NULL;
     }
-    vec_env->thread_data->num_threads = 0;
     free(vec_env->thread_data);
 }
 
@@ -401,9 +400,9 @@ static int c_vecstep(VecEnv* vec_env)
     // Produce work for the worker threads.
     atomic_int* work_index = &vec_env->thread_data->work_index;
     atomic_store_explicit(work_index, vec_env->num_envs - 1, memory_order_relaxed);
-    
+
     // Signal to other threads that there is new work to be done.
-    pthread_cond_broadcast(&vec_env->thread_data->wake_cnd);    
+    pthread_cond_broadcast(&vec_env->thread_data->wake_cnd);
 
     // Why waste a (main) thread? (Also no need for a lock/condition variable etc).
     int index;
