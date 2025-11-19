ddf28a96f2be44cf28f10225be580c4df3901de5
Peru S <perumaal@rlplays.com>
2025-11-18 17:35:46 -0800

    Comment out again

--- pufferlib/pufferl.py
+++ pufferlib/pufferl.py
@@ -949,19 +949,20 @@ def train(env_name, args=None, vecenv=None, policy=None, logger=None, should_sto
     while pufferl.global_step < train_config['total_timesteps']:
         if train_config['device'] == 'cuda':
             torch.compiler.cudagraph_mark_step_begin()
-        with torch.profiler.profile(
-            activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-            record_shapes=True, profile_memory = True,
-            with_stack=True
-        ) as prof:
-            with torch.profiler.record_function("evaluate"):
-               pufferl.evaluate()
-        ts = datetime.now().strftime("%Y_%m_%d_%H_%M")
-        profile_name = f"eval_{env_name}_{ts}.json"
-        prof.export_chrome_trace(profile_name)
-        print(f"Chrome trace exported to {profile_name}")
-        exit(0)
-        # pufferl.evaluate()
+        # Uncomment to profile evaluation using torch profiler, open using chrome://tracing or https://ui.perfetto.dev
+        # with torch.profiler.profile(
+        #     activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+        #     record_shapes=True, profile_memory = True,
+        #     with_stack=True
+        # ) as prof:
+        #     with torch.profiler.record_function("evaluate"):
+        #        pufferl.evaluate()
+        # ts = datetime.now().strftime("%Y_%m_%d_%H_%M")
+        # profile_name = f"eval_{env_name}_{ts}.json"
+        # prof.export_chrome_trace(profile_name)
+        # print(f"Chrome trace exported to {profile_name}")
+        # exit(0)
+        pufferl.evaluate()
         if train_config['device'] == 'cuda':
             torch.compiler.cudagraph_mark_step_begin()
         logs = pufferl.train()
