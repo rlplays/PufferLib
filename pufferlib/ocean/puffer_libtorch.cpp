74afb8d4a03cd0f261fa6b57263a59b54ad8aa20
Peru S <perumaal@rlplays.com>
2025-11-20 20:11:50 -0800

    Got a little bit of time tonight to test out tensors + libtorch!!

--- pufferlib/ocean/puffer_libtorch.cpp
+++ pufferlib/ocean/puffer_libtorch.cpp
@@ -18,18 +18,39 @@ void c_libtorch_info()
 
 struct LSTMWrapper : torch::nn::Module
 {
-  LSTMWrapper(const int input_size = 128, const int hidden_size = 128, const int obs_size, int logit_sizes[],
-              const int num_actions)
+  LSTMWrapper(const int input_size = 128, const int hidden_size = 128, const int obs_size,
+    int logit_sizes[], const int num_actions) :
+    hidden_size_(hidden_size), input_size_(input_size), obs_size_(obs_size), num_actions_(num_actions)
   {
-    // Need encoder and decoder.
+    // TODO: Assumes multidiscrete.
+    encoder = register_module("encoder", torch::nn::Sequential(
+      layer_init(torch::nn::Linear(obs_size, hidden_size)),
+      torch::nn::GELU()));
     lstm_cell = register_module("lstmcell", torch::nn::LSTMCell(input_size, hidden_size));
-    // lstm_cell->forward(torch::tensor)
+    decoder = register_module(
+      "decoder", torch::nn::Linear(hidden_size, std::accumulate(logit_sizes, logit_sizes + num_actions, 0)));
   }
 
-  int[] forward(const float* obs) {}
+  torch::nn::Linear layer_init(torch::nn::Linear layer, const double std = std::sqrt(2.0),
+    const double bias_const = 0.0)
+  {
+    torch::nn::init::orthogonal_(layer->weight, std);
+    torch::nn::init::constant_(layer->bias, bias_const);
+    return layer;
+  }
 
-  // For inference only.
+  void forward_eval(float* obs, float* actions_out)
+  {
+    // Assumes obs_size_ for obs, and num_actions_ for actions_out already initialized.
+    auto obs_tensor = torch::from_blob(obs, {obs_size_}, torch::kFloat32);
+    torch::Tensor hidden_tensor = encoder(obs_tensor);
+  }
+
+  // Inference only for now (need to copy weights from trained model)
+  torch::nn::Sequential encoder{nullptr};
   torch::nn::LSTMCell lstm_cell{nullptr};
+  torch::nn::Linear decoder{nullptr};
+  int hidden_size_, input_size_, obs_size_, num_actions_;
 };
 
 void c_eval() {}
