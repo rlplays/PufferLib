b4dd4d11fb0b15cec7ed2acae23b06a3b7ef06a4
Peru S <perumaal@rlplays.com>
2025-11-26 20:09:37 -0800

    Fix paths

--- pufferlib/puffer_pyapi.cpp
+++ pufferlib/puffer_pyapi.cpp
@@ -1,5 +1,21 @@
 // Include the CPP here so the tests etc don't have to pull in pybind etc.
 #include <puffer_libtorch.cpp>
+#include <pybind11.h>
 
+PYBIND11_MODULE(puffer_libtorch, m)
+{
+    m.doc() = "PufferLib Libtorch API";
+
+    m.def("libtorch_info", &pufferlib::c_libtorch_info, "Print libtorch info to stdout.");
+
+    pybind11::class_<pufferlib::PufferTorch>(m, "PufferTorch")
+        .def_static("alloc", &pufferlib::c_torch_alloc, "Allocate a PufferTorch model.", pybind11::arg("opt"))
+        .def("load_weights", &pufferlib::c_torch_load_weights, "Load weights into the model.", pybind11::arg("weights"))
+        .def("free", &pufferlib::c_torch_free, "Free the PufferTorch model.");
+
+    pybind11::class_<pufferlib::PufferEnvState>(m, "PufferEnvState")
+        .def_static("initenv", &pufferlib::c_initenv, "Initialize a new environment state.", pybind11::arg("pt"))
+        .def("freeenv", &pufferlib::c_freeenv, "Free the environment state.", pybind11::arg("pt"));
+}
 
 
--- setup.py
+++ setup.py
@@ -200,7 +200,7 @@ extension_kwargs = dict(
 c_extensions = []
 if not NO_OCEAN:
     c_extension_paths = glob.glob('pufferlib/ocean/**/binding.c', recursive=True)
-    c_extension_paths += ['pufferlib/ocean/puffer_pyapi.cpp']
+    c_extension_paths += ['pufferlib/puffer_pyapi.cpp']
     c_extensions = [
         Extension(
             path.rstrip('.c').rstrip('.cpp').replace('/', '.'),
