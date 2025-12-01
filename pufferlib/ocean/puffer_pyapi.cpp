f308c6d33ec5ee46f1c7e93f9ecdf84f4a204f65
Peru S <perumaal@rlplays.com>
2025-12-01 00:36:36 -0800

    Remove extra code

--- pufferlib/ocean/puffer_pyapi.cpp
+++ pufferlib/ocean/puffer_pyapi.cpp
@@ -4,20 +4,11 @@
 
 extern "C" PyMethodDef* get_methods();
 
-// NumPy import_array needs special handling - it may return or set error
-static int init_numpy() {
-    import_array1(-1);
-    return 0;
-}
-
 PYBIND11_MODULE(binding, m)
 {
     m.doc() = "PufferLib Libtorch API";
 
-    if (init_numpy() < 0) {
-        throw pybind11::error_already_set();
-    }
-
+    import_array();
     PyModule_AddFunctions(m.ptr(), get_methods());
     m.def("libtorch_info", &c_libtorch_info, "Print libtorch info to stdout.");
 
