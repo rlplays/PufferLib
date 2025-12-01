0d6d137e9aacb3944af42516085f28a9b7300168
Peru S <perumaal@rlplays.com>
2025-12-01 01:01:13 -0800

    Print a simple tensor from Py->C++ and bind correctly

--- pufferlib/ocean/puffer_pyapi.cpp
+++ pufferlib/ocean/puffer_pyapi.cpp
@@ -1,6 +1,7 @@
 // Include the CPP here so the tests etc don't have to pull in pybind etc.
 #include <puffer_libtorch.cpp>
 #include <pybind11/pybind11.h>
+#include <torch/extension.h>
 
 // Suggested by Claude to avoid pybind/C++ using import_array/numpy here while env_binding uses just the PyAPI alone (using non pybind).
 #define PY_ARRAY_UNIQUE_SYMBOL puffer_ARRAY_API
@@ -16,8 +17,7 @@ PYBIND11_MODULE(binding, m)
     import_array();
     PyModule_AddFunctions(m.ptr(), get_methods());
     m.def("libtorch_info", &c_libtorch_info, "Print libtorch info to stdout.");
-    m.def("print_tensor_info", &c_print_tensor_info, "Print tensor info to stdout.");
-
+    m.def("print_tensor_info", &c_print_tensor_info, py::arg("tensor"), "Print tensor info to stdout.");
 }
 
 
