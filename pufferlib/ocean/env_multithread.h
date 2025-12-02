a8e3d21ba2261ecad31dca99ce2713fdc7489e5c
Peru S <perumaal@rlplays.com>
2025-12-02 14:58:36 -0800

    Make 'em all static

--- pufferlib/ocean/env_multithread.h
+++ pufferlib/ocean/env_multithread.h
@@ -30,18 +30,48 @@ typedef struct
   struct PufferEnvState** env_states;
 } VecEnv;
 
-void c_step_wrapper(Env* env, struct PufferTorch* pt, struct PufferEnvState* env_state)
+
+static struct PufferOptions global_options = {0};
+static void (*c_funcstep)(Env*, struct PufferTorch*, struct PufferEnvState*) = NULL;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+static struct PufferEnvState* get_envstate(VecEnv* vec_env, int env_index)
+{
+  if (!vec_env->env_states) { return NULL; }
+  return vec_env->env_states[env_index];
+}
+
+static struct PufferTorch* get_puffertorch(VecEnv* vec_env) { return vec_env->puff_torch; }
+
+static int get_numenvstates(VecEnv* vec_env)
+{
+  if (!vec_env->env_states) { return 0; }
+  return vec_env->num_envs;
+}
+
+static float* get_obs_ptr(Env* env) { return env->observations; }
+static int* get_actions_ptr(Env* env) { return env->actions; }
+static float* get_rewards_ptr(Env* env) { return env->rewards; }
+static unsigned char* get_terminals_ptr(Env* env) { return env->terminals; }
+
+  
+static void c_step_wrapper(Env* env, struct PufferTorch* pt, struct PufferEnvState* env_state)
 {
   c_step(env);
 }
 
-static struct PufferOptions global_options = {0};
-static void (*c_funcstep)(Env*, struct PufferTorch*, struct PufferEnvState*) = c_step_wrapper;
-
-void c_set_funcstep(void (*func)(Env*, struct PufferTorch*, struct PufferEnvState*))
+static void c_set_funcstep(void (*func)(Env*, struct PufferTorch*, struct PufferEnvState*))
 {
   c_funcstep = func;
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 // Main worker thread; initializes itself and runs a tight loop running through c_step (after waiting for work signal).
 static void* c_threadstep(void* arg)
@@ -202,30 +232,3 @@ static int c_vecstep(VecEnv* vec_env)
   return 0;
 }
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-struct PufferEnvState* get_envstate(VecEnv* vec_env, int env_index)
-{
-  if (!vec_env->env_states) { return NULL; }
-  return vec_env->env_states[env_index];
-}
-
-struct PufferTorch* get_puffertorch(VecEnv* vec_env) { return vec_env->puff_torch; }
-
-int get_numenvstates(VecEnv* vec_env)
-{
-  if (!vec_env->env_states) { return 0; }
-  return vec_env->num_envs;
-}
-
-float* get_obs_ptr(Env* env) { return env->observations; }
-int* get_actions_ptr(Env* env) { return env->actions; }
-float* get_rewards_ptr(Env* env) { return env->rewards; }
-unsigned char* get_terminals_ptr(Env* env) { return env->terminals; }
-
-#ifdef __cplusplus
-}
-#endif
