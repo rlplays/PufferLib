694e577177cc96bc9ca25c9b24b4ab5ac752c108
Peru S <perumaal@rlplays.com>
2025-12-06 11:36:02 -0800

    Move / simplify stuff out so the env_multithread is really just env_glue?

--- pufferlib/ocean/env_multithread.h
+++ pufferlib/ocean/env_multithread.h
@@ -1,69 +1,14 @@
 #ifdef __cplusplus
 #pragma once
-#include <atomic>
-#include <thread>
-#include <condition_variable>
-#include <mutex>
-
-#else
-#include <stdatomic.h>
-#include <pthread.h>
 #endif
 
 #include "puffer_nativecpp.h"
 
+// puffer_nativecpp.cpp is compiled as a separate unit so we need to glue these here. (Env is not visible outside the env's binding.c).
+// TODO: Env should really be a well-defined struct in its own header instead of #define'd inside the env ?
 float* get_obs_ptr(Env* env) { return env->observations; }
 int* get_actions_ptr(Env* env) { return env->actions; }
 float* get_rewards_ptr(Env* env) { return env->rewards; }
 unsigned char* get_terminals_ptr(Env* env) { return env->terminals; }
 
-//! @brief Inits vectorized multi-threading envs with provided num threads. Returns 0 on success (1 on error).
-static int c_vecinit(struct VecEnv* vec_env)
-{
-  // If we have only a couple envs, it's not worth parallelizing. Also, don't penalize the user as they
-  // may want to change the .ini dynamically without having to worry about this.
-  if (vec_env->opts.num_threads <= 2 || vec_env->num_envs <= 2)
-  {
-    vec_env->opts.num_threads = 0;
-    return 1;
-  }
-  c_init_multithreading(vec_env);
-  if (vec_env->opts.enable_native_libtorch)
-  {
-    vec_env->puff_torch = c_torch_alloc(vec_env);
-  }
-  else
-  {
-    vec_env->puff_torch = NULL;
-  }
-  return 0;
-}
-
-//! @brief Waits for and exits all threads (if needed).
-static void c_vecclose(struct VecEnv* vec_env)
-{
-  c_shutdown_multithreading(vec_env);
-
-  if (vec_env->puff_torch)
-  {
-    c_torch_free(vec_env->puff_torch);
-    vec_env->puff_torch = NULL;
-  }
-}
-
 void c_single_step(void* vec_env, int index) { c_step(((VecEnv*)vec_env)->envs[index]); }
-
-//! @brief Old multithreaded step function for vec envs without native libtorch support.
-//! Returns 0 on success (1 on error).
-static int c_vecstep(struct VecEnv* vec_env)
-{
-  if (vec_env->opts.enable_native_libtorch)
-  {
-    // Must use the c_native_fulleval instead that does action (inference) + step segmented across a BPTT horizon.
-    return 1;
-  }
-  c_start_work(vec_env);
-  c_add_work_batched(vec_env, c_single_step, vec_env, 0, vec_env->num_envs - 1);
-  c_wait_all_done(vec_env);
-  return 0;
-}
--- pufferlib/ocean/puffer_nativecpp.h
+++ pufferlib/ocean/puffer_nativecpp.h
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #endif
+
+
 #ifndef PUFFER_NATIVECPP_H
 #define PUFFER_NATIVECPP_H
 #if defined(DEBUG)
@@ -117,8 +119,61 @@ void c_add_work_batched(struct VecEnv* vec_env, work_func func, void* arg, int s
 //! @brief Waits for all queued work to be done.
 void c_wait_all_done(struct VecEnv* vec_env);
 
+
+//! @brief Inits vectorized multi-threading envs with provided num threads. Returns 0 on success (1 on error).
+static int c_vecinit(struct VecEnv* vec_env)
+{
+  // If we have only a couple envs, it's not worth parallelizing. Also, don't penalize the user as they
+  // may want to change the .ini dynamically without having to worry about this.
+  if (vec_env->opts.num_threads <= 2 || vec_env->num_envs <= 2)
+  {
+    vec_env->opts.num_threads = 0;
+    return 1;
+  }
+  c_init_multithreading(vec_env);
+  if (vec_env->opts.enable_native_libtorch)
+  {
+    vec_env->puff_torch = c_torch_alloc(vec_env);
+  }
+  else
+  {
+    vec_env->puff_torch = NULL;
+  }
+  return 0;
+}
+
+//! @brief Waits for and exits all threads (if needed).
+static void c_vecclose(struct VecEnv* vec_env)
+{
+  c_shutdown_multithreading(vec_env);
+
+  if (vec_env->puff_torch)
+  {
+    c_torch_free(vec_env->puff_torch);
+    vec_env->puff_torch = NULL;
+  }
+}
+
+void c_single_step(void* vec_env, int index);
+
+//! @brief Old multithreaded step function for vec envs without native libtorch support.
+//! Returns 0 on success (1 on error).
+static int c_vecstep(struct VecEnv* vec_env)
+{
+  if (vec_env->opts.enable_native_libtorch)
+  {
+    // Must use the c_native_fulleval instead that does action (inference) + step segmented across a BPTT horizon.
+    return 1;
+  }
+  c_start_work(vec_env);
+  c_add_work_batched(vec_env, c_single_step, vec_env, 0, vec_env->num_envs - 1);
+  c_wait_all_done(vec_env);
+  return 0;
+}
+
 #if defined(__cplusplus)
 }
 #endif
 
-#endif
+#endif // PUFFER_NATIVECPP_H
+
