2109acd77a38059e36c0a62696dc07eb41afbd41
Peru S <perumaal@rlplays.com>
2025-12-09 14:57:11 -0800

    Return full horizon state

--- pufferlib/models.py
+++ pufferlib/models.py
@@ -193,8 +193,7 @@ class LSTMWrapper(nn.Module):
         '''Finishes the native libtorch eval (per epoch).'''
         vecenvs = backend.get_vecenvs()
         binding = backend.get_binding()
-        # TODO: Provide all of our holders here to obtain the final states.
-        binding.torch_finish_eval_lstm(vecenvs)
+        return binding.torch_finish_eval_lstm(vecenvs)
 
     def forward(self, observations, state):
         '''Forward function for training. Uses LSTM for fast time-batching'''
