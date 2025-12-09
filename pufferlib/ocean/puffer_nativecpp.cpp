890cd44d4b61c19d967dbe53ba734a516699f87a
Peru S <perumaal@rlplays.com>
2025-12-08 18:21:39 -0800

    Done  for the day...

--- pufferlib/ocean/puffer_nativecpp.cpp
+++ pufferlib/ocean/puffer_nativecpp.cpp
@@ -344,24 +344,21 @@ struct LSTMWrapper : torch::nn::Module
   {
     BEGIN_LIBTORCH_CATCH
     {
-      //for (int segment = 0; segment < opt->bptt_horizon; segment++)
-      {
-        torch::NoGradGuard no_grad;
+      torch::NoGradGuard no_grad;
 
-        c_start_work(vec_env);
-        // Kick off this batch of work.
-        // TODO: Should we do each batch-segment part of this horizon independently? or all at once?
-        // We can start off with putting this whole thing in a for loop (i.e. each iteration, wait for all done) to begin with.
-        // I think ideally, some stuff should just start going forward.
-        c_add_work_batched(vec_env, run_next_bptt_segment, this, 0, eval_batch_count - 1);
-        // full_obs is [num_envs, obs_size] in CPU side.
-        // Transfer each obs batch to device independently.
-        // Add batch work: torch_batch_eval(this, index)
-        // Get the action[]/etc tensors from each batch.
-        // Enqueue the env steps.
-        // cat all tensors and return.
-        c_wait_all_done(vec_env);
-      }
+      c_start_work(vec_env);
+      // Kick off this batch of work.
+      // TODO: Should we do each batch-segment part of this horizon independently? or all at once?
+      // We can start off with putting this whole thing in a for loop (i.e. each iteration, wait for all done) to begin with.
+      // I think ideally, some stuff should just start going forward.
+      c_add_work_batched(vec_env, run_next_bptt_segment, this, 0, eval_batch_count - 1);
+      // full_obs is [num_envs, obs_size] in CPU side.
+      // Transfer each obs batch to device independently.
+      // Add batch work: torch_batch_eval(this, index)
+      // Get the action[]/etc tensors from each batch.
+      // Enqueue the env steps.
+      // cat all tensors and return.
+      c_wait_all_done(vec_env);
     }
     END_LIBTORCH_CATCH
   }
