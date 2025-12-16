be8d3181981486d7489f7b726ce46f420ed81f93
Peru S <perumaal@rlplays.com>
2025-12-16 15:19:30 -0800

    c

--- pufferlib/ocean/env_glue.h
+++ pufferlib/ocean/env_glue.h
@@ -36,7 +36,7 @@ void c_step_batch(void* arg, int env_index, void* actions_data, int num_actions,
 
   // Doing rewards/terminals here also maintains cache locality as the env step just wrote to these pointers.
   float r = env->rewards[0];
-  r = max(-1.0f, std::min(1.0f, r));
+  r = max(-1.0f, min(1.0f, r));
   rewards[env_index] = r;
   terminals[env_index] = (env->terminals[0] != 0 ? 1.0f : 0.0f);
   
