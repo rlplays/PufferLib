a368dd7fd10159ee677110abd037a9012ba6694d
Peru S <perumaal@rlplays.com>
2025-12-17 12:56:48 -0800

    Rename / also create experiments dir

--- scripts/profile_envs.sh
+++ scripts/profile_envs.sh
@@ -2,6 +2,7 @@
 # e.g. usage: bash profile_env.sh puffer_go,puffer_breakout,puffer_squared --profile.name multiproc --profile.train 0
 
 IFS=',' read -ra envs <<< "$1"
+mkdir -p experiments/
 for env in "${envs[@]}"; do
    echo "Using env: $env"
    echo "python -m pufferlib.pufferl profile $env --train.device cuda ${@:2}"
