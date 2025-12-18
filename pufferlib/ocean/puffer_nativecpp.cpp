b0b256005e2fb381bd1d57da0ada4eea23a18351
Peru S <perumaal@rlplays.com>
2025-12-18 15:01:19 -0800

    Comment

--- pufferlib/ocean/puffer_nativecpp.cpp
+++ pufferlib/ocean/puffer_nativecpp.cpp
@@ -22,6 +22,7 @@ constexpr bool global_debug_mode = false;
 constexpr bool global_cuda_async = true;
 // Enable multiple streams per batch by default. 2 means double-buffering etc.
 // Do not set this to a large number since the memory gets fragmented/reserved unnecessarily resulting in OOMs.
+// Very useful doc: https://docs.pytorch.org/docs/stable/notes/cuda.html#memory-management
 constexpr int global_num_cuda_streams_per_batch = 4;
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/cuda/CUDAStream.h>
