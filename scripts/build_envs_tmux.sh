79e62a5df408504f9ebf77d38d723ef92fef5d9d
Peru S <perumaal@rlplays.com>
2025-12-18 13:55:31 -0800

    Remove tmux script

--- scripts/build_envs_tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IFS=',' read -ra envs <<< "$1"
-for env in "${envs[@]}"; do
-  echo "Building env: $env in tmux pane"
-  tmux split-window -h "python setup.py build_$env --inplace --force || read -r -p 'Build failed for $env. Press Enter to continue...'"
-done
