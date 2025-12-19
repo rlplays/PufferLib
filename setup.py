229ee5ffd9e1ff6015db9beee72d540c35cbd53d
Peru S <perumaal@rlplays.com>
2025-12-19 10:55:26 -0800

    Fix

--- setup.py
+++ setup.py
@@ -237,7 +237,7 @@ if not NO_OCEAN:
             **extension_kwargs,
         )
         # TODO(perumaal): For now, just build go/breakout for testing purposes.
-        for path in c_extension_paths if 'breakout' in path or 'go' in path
+        for path in c_extension_paths if '/breakout' in path or '/go' in path
     ]
     c_extension_paths = [os.path.join(*path.split('/')[:-1]) for path in c_extension_paths]
 
