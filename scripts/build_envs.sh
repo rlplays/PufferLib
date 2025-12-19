38bc4fc91b554554b53b52051065bbf4c5c36be3
Peru S <perumaal@rlplays.com>
2025-12-19 10:27:47 -0800

    Fix config / script

--- scripts/build_envs.sh
+++ scripts/build_envs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+python setup.py build_torch
 IFS=',' read -ra envs <<< "$1"
 for env in "${envs[@]}"; do
+
   echo "Building env: $env"
   python setup.py build_"$env" --inplace --force || read -r -p "Build failed for $env. Press Enter to continue..." _
 done
