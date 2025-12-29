7aac1e76f40137b7144fffd8b5821d92e2df4382
Peru S <perumaal@rlplays.com>
2025-12-29 14:14:13 -0800

    Add CUDA memory profiling support with single threaded mode

--- setup.py
+++ setup.py
@@ -45,8 +45,11 @@ NO_OCEAN = os.getenv("NO_OCEAN", "0") == "1"
 NO_TRAIN = os.getenv("NO_TRAIN", "0") == "1"
 NO_TORCH = os.getenv("NO_TORCH", "0") == "1"
 NO_ASAN = os.getenv("NO_ASAN", "0") == "1"
+SINGLE_THREADED = os.getenv("SINGLE_THREADED", "0") == "1"
 
 print(f"------- DEBUG MODE? {DEBUG} -------------")
+if SINGLE_THREADED:
+    print("------- SINGLE THREADED MODE! -------------")
 # Build raylib for your platform
 RAYLIB_URL = 'https://github.com/raysan5/raylib/releases/download/5.5/'
 RAYLIB_NAME = 'raylib-5.5_macos' if platform.system() == "Darwin" else 'raylib-5.5_linux_amd64'
@@ -153,6 +156,10 @@ else:
     ]
     nvcc_args += [
         '-O2',
+    ]
+if SINGLE_THREADED:
+    extra_compile_args += [
+        '-DPUFFER_SINGLE_THREADED',
     ]
 
 system = platform.system()
