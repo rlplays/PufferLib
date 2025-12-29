28b1e3ca57615ee38de1ed7bda5d8ec49180d391
Peru S <perumaal@rlplays.com>
2025-12-29 12:52:57 -0800

    Fix duration ms

--- pufferlib/ocean/puffer_native.cpp
+++ pufferlib/ocean/puffer_native.cpp
@@ -416,7 +416,7 @@ struct LSTMWrapper : torch::nn::Module
         // Prepare for next run.
         state->lstm_wrapper = nullptr;
       }
-      result.stats_millis.push_back({perf_total_forward_eval.name, perf_total_forward_eval.get_duration_ms()});
+      result.stats_millis.push_back({perf_total_forward_eval.name, perf_total_forward_eval.get_duration_millis()});
       result.step_count = this->horizon_steps;
       result.total_steps = this->total_steps;
       vec_env = nullptr;
