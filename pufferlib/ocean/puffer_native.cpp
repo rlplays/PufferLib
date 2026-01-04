5606aa54ba848e536c4af81bed705f605d599184
Peru S <perumaal@rlplays.com>
2026-01-03 16:56:07 -0800

    c

--- pufferlib/ocean/puffer_native.cpp
+++ pufferlib/ocean/puffer_native.cpp
@@ -864,14 +864,14 @@ private:
 
 #if PUFFER_DBG_CHECK_NETWORK_SLOW
         {
-          auto actions_horizon_copy = state->actions_horizon[segment].clone();
-          auto logprob_horizon_copy = state->logprob_horizon[segment].clone();
+          auto actions_horizon_copy = state->actions_horizon[segment].clone().zero_();
+          auto logprob_horizon_copy = state->logprob_horizon[segment].clone().zero_();
           sample_logits(logits, opt->num_actions, opt->logit_sizes, actions_horizon_copy, logprob_horizon_copy);
           // Don't compare - as the sampling is non-deterministic even with a fixed random seed (as the random values are pre-generated).
-          //c_compare_tensorsi(actions_horizon_copy, "OLD sample_logits_actions", state->actions_horizon[segment],
-          //  "NEW fused", true);
-          //c_compare_tensorsf(logprob_horizon_copy, "OLD sample_logits_logprobs", state->logprob_horizon[segment],
-          //  "NEW fused", true);
+          c_compare_tensorsi(actions_horizon_copy, "OLD sample_logits_actions", state->actions_horizon[segment],
+            "NEW fused", true);
+          c_compare_tensorsf(logprob_horizon_copy, "OLD sample_logits_logprobs", state->logprob_horizon[segment],
+            "NEW fused", true);
         }
 #endif
 
