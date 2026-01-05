b71c013bd2eba876c9ff9343fe012cdba42709af
Peru S <perumaal@rlplays.com>
2026-01-04 17:14:46 -0800

    Add test

--- pufferlib/ocean/puffer_cuda.cpp
+++ pufferlib/ocean/puffer_cuda.cpp
@@ -4,12 +4,14 @@
 #include <torch/extension.h>
 
 
-
 PYBIND11_MODULE(native, m)
 {
   m.doc() = "PufferLib native CUDA API for testing purposes.";
-  m.def("launch_linear_forward", &launch_linear_forward, py::arg("input"), 
-      py::arg("weight"), py::arg("bias"), py::arg("output"),
-    "Launches a linear forward cuda kernel.");
+  m.def("launch_dual_linear_forward", &launch_dual_linear_forward, py::arg("input"), py::arg("weight1"),
+        py::arg("bias1"), py::arg("output1"), py::arg("weight2"), py::arg("bias2"), py::arg("output2"),
+        "Launches a dual linear forward that outputs decoder (logits) + values.");
+  m.def("launch_sample_logits_kernel", &launch_sample_logits_kernel, py::arg("random_vals"), py::arg("sizes_gpu"),
+        py::arg("offsets_gpu"), py::arg("logits"), py::arg("num_actions"), py::arg("logit_sizes"), py::arg("actions"),
+        py::arg("logprobs"),
+        "Launches a logits kernels that samples actions into output actions and assigns logprobs for those actions.");
 }
-
--- scripts/test-cuda-kernels.py
+++ scripts/test-cuda-kernels.py
@@ -1,14 +1,32 @@
 import torch
 import pufferlib.native as nativelib
 
-R = 131
-C = 101
-CR = 31
-output = torch.zeros(R, C).cuda()
-nativelib.launch_linear_forward(torch.ones(R, CR).cuda(),
-                                torch.ones(C, CR).cuda(),
-                                torch.zeros(R, C).cuda(),
-                                output
+
+
+def test_sample_logits():
+  B = 10
+  A = 5
+  logprobs = torch.zeros(B).cuda()
+  actions = torch.zeros(B, A, dtype=torch.int64).cuda()
+  nativelib.launch_sample_logits(torch.randn(B, A).cuda(),
+                                torch.rand(B, A).cuda(),
+                                logprobs,
+                                actions
                                 )
- 
-print(f"Output : {output}")
+   
+  print(f"Logprobs : {logprobs}")
+  print(f"Actions : {actions}")
+
+
+def test_linear_forward():
+  R = 131
+  C = 101
+  CR = 31
+  output = torch.zeros(R, C).cuda()
+  nativelib.launch_linear_forward(torch.ones(R, CR).cuda(),
+                                  torch.ones(C, CR).cuda(),
+                                  torch.zeros(R, C).cuda(),
+                                  output
+                                  )
+   
+  print(f"Output : {output}")
