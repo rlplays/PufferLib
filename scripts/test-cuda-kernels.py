fabd4be4fb8c17ab5749283512584fa21c2410c2
Peru S <perumaal@rlplays.com>
2026-01-04 17:33:13 -0800

    Debugging/understanding the kernels

--- scripts/test-cuda-kernels.py
+++ scripts/test-cuda-kernels.py
@@ -4,11 +4,11 @@ import pufferlib.native as nativelib
 
 
 def test_sample_logits():
-  N = 64
+  N = 42
   A = 3
   logprobs = torch.zeros(N).fill_(2.0).cuda()
   actions = torch.zeros(N, A, dtype=torch.int64).fill_(1).cuda()
-  logits = torch.randn(N*3).cuda()
+  logits = torch.randn(N, 6).cuda()
   nativelib.launch_sample_logits_kernel(torch.randn(N, A).cuda(),
                                 torch.tensor([2, 2, 2], dtype=torch.int64).cuda(),
                                 torch.tensor([0, 2, 4], dtype=torch.int64).cuda(),
@@ -17,8 +17,8 @@ def test_sample_logits():
                                 actions,
                                 logprobs)
    
-  print(f"Logprobs : {logprobs.cpu()}")
   print(f"Actions : {actions.cpu()}")
+  print(f"Logprobs : {logprobs.cpu()}")
 
 
 test_sample_logits()
