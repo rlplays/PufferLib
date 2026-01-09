d33291829096d8fe87830afa45dbf79ad8dc1c26
Peru S <perumaal@rlplays.com>
2026-01-09 11:08:59 -0800

    Test done

--- tests/test_native_sample_logits.py
+++ tests/test_native_sample_logits.py
@@ -9,11 +9,13 @@ def test_sample_logits():
   full_logprobs_out = torch.zeros(100, 64).cuda()
 
   for j in range(10):
-    val = j * 0.1
+    val = j * 0.1 # Probability values [0, 1]
     full_logits.fill_(val)
-    full_actions_out.fill_(2345)
-    full_logprobs_out.fill_(-1.424242)
+    full_actions_out.fill_(2345) # Sentinel
+    full_logprobs_out.fill_(-1.424242) # Sentinel
     torch.manual_seed(42)
+
+    # Get C++ logprobs/actions
     logits = full_logits.select(1, 10)
     actions_out = full_actions_out.select(1, 10)
     logprobs_out = full_logprobs_out.select(1, 10)
@@ -22,6 +24,20 @@ def test_sample_logits():
     print_tensor(actions_out.cpu(), "C++ Actions Out", True)
     print_tensor(logprobs_out.cpu(), "C++ Logprobs Out", True)
 
+    # CUDA version test in test_cuda_kernels.py
+    # The test here only verifies that the raw (unused/existing) C++ version matches the PyTorch version.
+    # The C++ version was converted to a CUDA kernel manually.
+
+    # New PyTorch version test
+    torch.manual_seed(42)
+    actions1, logprobs1, _ = puffypy.sample_logits_v2(logits, 1, [50])
+    print_tensor(actions1.cpu(), "Py Actions Out v2", True)
+    print_tensor(logprobs1.cpu(), "Py Logprobs Out v2", True)
+    verify_tensor = torch.eq(actions_out, actions1).all() and torch.allclose(logprobs_out, logprobs1)
+    print(f"Verification : {verify_tensor}")
+    assert(verify_tensor)
+
+    # Old PyTorch version test
     torch.manual_seed(42)
     actions2, logprobs2, _ = puffypy.sample_logits(logits, 1, [50])
     print_tensor(actions2.cpu(), "Py Actions Out", True)
