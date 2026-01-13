f8697ff5cbd13c466858f4f59b1f24791277948c
Peru S <perumaal@rlplays.com>
2026-01-12 19:25:42 -0800

    Fix comment

--- pufferlib/ocean/puffer_native.cpp
+++ pufferlib/ocean/puffer_native.cpp
@@ -524,12 +524,13 @@ struct LSTMWrapper : torch::nn::Module
       perf_total_forward_eval.start();
       num_batches_done = 0;
       c_start_work(vec_env);
-      // We can start off with putting this whole thing in a for loop (i.e. each iteration, wait for all done) to begin
-      // with. I think ideally, some stuff should just start going forward.
+      // Start with segment 0 for each batch. Once each one is done, it will enqueue the next segment 
+      // until all segments are done.
       add_work_batched(vec_env, run_next_bptt_segment, this, 0, eval_batch_count - 1,
         /* batch_completion*/ nullptr, /* min_num_items_per_batch */ 1, PufferWorkType::BatchWork);
 
-      // Note because different threads may enqueue work, the queue(s) might be empty intermittently, so the c_wait_all_done may exit prematurely..
+      // Note because different threads may enqueue work, the queue(s) might be empty intermittently, 
+      // so the c_wait_all_done may exit prematurely...
       c_wait_all_done(vec_env);
 
       // ...so block the main thread and wait here until the batches are done.
