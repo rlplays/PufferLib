9487796545c3c3a2516c05d3a1a2b29e95bbf6d9
Peru S <perumaal@rlplays.com>
2026-03-23 11:40:24 -0700

    Allow grid too

--- setup.py
+++ setup.py
@@ -293,7 +293,7 @@ if not NO_OCEAN:
             language='c++',
             **extension_kwargs,
         )
-        for path in c_extension_paths if '/breakout' in path or '/go' in path or '/g2048' in path or '/pacman' in path or '/blastar' in path or '/pong' in path
+        for path in c_extension_paths if '/breakout' in path or '/grid' in path or '/go' in path or '/g2048' in path or '/pacman' in path or '/blastar' in path or '/pong' in path
     ]
     c_extension_paths = [os.path.join(*path.split('/')[:-1]) for path in c_extension_paths]
 
